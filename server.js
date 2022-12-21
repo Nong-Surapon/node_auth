fc98a71fe5386acc91b989705f37973165b5f749
Surapon Thongnen <modernlife1988@gmail.com>
2022-12-21 22:07:35 +0700

    Added express server

--- server.js
+++ server.js
@@ -1,111 +1,98 @@
-const http = require('http')
+const express = require('express')
+const app = express()
 const path = require('path')
-const fs = require('fs')
-const fsPromises = require('fs').promises
-
-const logEvents = require('./logEvents')
-const EventEmitter = require('events')
-class Emitter extends EventEmitter {}
-// initalize object
-const myEmitter = new Emitter()
-myEmitter.on('log', (msg, fileName) => logEvents(msg, fileName))
-
+const cors = require('cors')
+const { logger } = require('./middleward/logEvents')
+const errorHandle = require('./middleward/errorHandle')
 const PORT = process.env.PORT || 3500
 
-const serveFile = async (filePath, contentType, response) => {
-  try {
-    const rawData = await fsPromises.readFile(
-      filePath,
-      !contentType.includes('image') ? 'utf8' : '',
-    )
-    const data =
-      contentType === 'application/json' ? JSON.parse(rawData) : rawData
-    response.writeHead(filePath.includes('404.html') ? 404 : 200, {
-      'Content-Type': contentType,
-    })
-    response.end(
-      contentType === 'application/json' ? JSON.stringify(data) : data,
-    )
-  } catch (err) {
-    console.log(err)
-    myEmitter.emit('log', `${err.name}: ${err.message}`, 'errLog.txt')
-    response.statusCode = 500
-    response.end()
-  }
+// custom middleware logger
+app.use(logger)
+
+// Cross Origin Resource Sharing
+const whitelist = [
+  'https://www.google.com',
+  'http://127.0.0.1:5500',
+  'http://localhost:3500',
+]
+const corsOptions = {
+  origin: (origin, callback) => {
+    if (whitelist.indexOf(origin) !== -1 || !origin) {
+      callback(null, true)
+    } else {
+      callback(new Error('Not allowed by CORS'))
+    }
+  },
+  optionsSuccessStatus: 200,
+}
+app.use(cors(corsOptions))
+
+// build-in middleware to handle urlencoded data
+// in other words, data:
+// content-type: application/x-www-form-urlencoded
+app.use(express.urlencoded({ extended: false }))
+
+// built-in middleware for json
+app.use(express.json())
+
+// server static files
+app.use(express.static(path.join(__dirname, '/public')))
+
+app.get('^/$|/index(.html)?', (req, res) => {
+  // res.sendFile('./views/index.html', { root: __dirname })
+  res.sendFile(path.join(__dirname, 'views', 'index.html'))
+})
+
+app.get('/new-page(.html)?', (req, res) => {
+  res.sendFile(path.join(__dirname, 'views', 'new-page.html'))
+})
+
+app.get('/old-page(.html)?', (req, res) => {
+  res.redirect(301, '/new-page.html') // 302 by default
+})
+
+// Route handles
+app.get(
+  '/hello(.html)?',
+  (req, res, next) => {
+    console.log('attempted to load hello.html')
+    next()
+  },
+  (req, res) => {
+    res.send('Hello World!')
+  },
+)
+
+// chaining route handles
+const one = (req, res, next) => {
+  console.log('one')
+  next()
 }
 
-const server = http.createServer((req, res) => {
-  console.log(req.url, req.method)
-  console.log('log', `${req.url}\t${req.method}`)
-  myEmitter.emit('log', `${req.url}\t${req.method}`, 'reqLog.txt')
+const two = (req, res, next) => {
+  console.log('two')
+  next()
+}
 
-  const extension = path.extname(req.url)
+const three = (req, res) => {
+  console.log('three')
+  res.send('Finished!')
+}
 
-  let contentType
+app.get('/chain(.html)?', [one, two, three])
 
-  switch (extension) {
-    case '.css':
-      contentType = 'text/css'
-      break
-    case '.js':
-      contentType = 'text/javascript'
-      break
-    case '.json':
-      contentType = 'application/json'
-      break
-    case '.jpg':
-      contentType = 'image/jpeg'
-      break
-    case '.png':
-      contentType = 'image/png'
-      break
-    case '.txt':
-      contentType = 'text/plain'
-      break
-    default:
-      contentType = 'text/html'
-  }
-
-  let filePath =
-    contentType === 'text/html' && req.url === '/'
-      ? path.join(__dirname, 'views', 'index.html')
-      : contentType === 'text/html' && req.url.slice(-1) === '/'
-      ? path.join(__dirname, 'views', req.url, 'index.html')
-      : contentType === 'text/html'
-      ? path.join(__dirname, 'views', req.url)
-      : path.join(__dirname, req.url)
-
-  // makes .html extension not required in the browser
-  if (!extension && req.url.slice(-1) !== '/') filePath += '.html'
-
-  const fileExists = fs.existsSync(filePath)
-
-  if (fileExists) {
-    serveFile(filePath, contentType, res)
+// Handle 404 not found
+app.all('*', (req, res) => {
+  res.status(404)
+  if (req.accepts('html')) {
+    res.sendFile(path.join(__dirname, 'views', '404.html'))
+  } else if (req.accepts('json')) {
+    res.json({ error: '404 Not Found' })
   } else {
-    switch (path.parse(filePath).base) {
-      case 'old-page.html':
-        res.writeHead(301, { Location: '/new-page.html' })
-        res.end()
-        break
-      case 'www-page.html':
-        res.writeHead(301, { Location: '/' })
-        res.end()
-        break
-      default:
-        serveFile(path.join(__dirname, 'views', '404.html'), 'text/html', res)
-    }
+    res.type('txt').send('404 Not Found')
   }
 })
 
-server.listen(PORT, () => console.log(`Server running on port ${PORT}`))
+app.use(errorHandle)
 
-/*
-// add listener for the log event
-emitter.on('log', (msg) => logEvents(msg))
-
-setTimeout(() => {
-  // Emit event
-  emitter.emit('log', 'Log event emitted!')
-}, 2000)
-*/
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
