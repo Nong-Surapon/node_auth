c1c1aeeca58fbfca5605100ff837bf28a70fb316
Surapon Thongnen <osprior_surapon.thongnen@krungthai.com>
2023-01-06 17:01:21 +0700

    Added verlify roles permistion

--- controllers/refreshTokenController.js
+++ controllers/refreshTokenController.js
@@ -26,8 +26,14 @@ const handleRefreshToken = (req, res) => {
       return res.sendStatus(403)
     }
 
+    const roles = Object.values(foundUser.roles)
     const accessToken = jwt.sign(
-      { username: decoded.username },
+      {
+        UserInfo: {
+          username: foundUser.username,
+          roles: roles,
+        },
+      },
       process.env.ACCESS_TOKEN_SECRET,
       { expiresIn: '30s' },
     )
